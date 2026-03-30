2f01b840c15f3930434ca0acdb2f113a42d9d34c
abhishektw456 <abhishek.singh@thoughtwin.com>
2026-03-30 16:23:27 +0530

    added rating in workmodel etc2

--- src/models/Donework.js
+++ src/models/Donework.js
@@ -38,6 +38,7 @@ const doneworkSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "Rating",
       required: false,
+      default: null,
     },
   },
   { timestamps: true },
